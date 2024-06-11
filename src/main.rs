d9124ecf6f136a12578f3e9790d37baf85a79adc
rakivo <awfulmacro@gmail.com>
2024-06-11 21:08:51 +0300

    Add support for $d[x] syntax, proper error reporting

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,18 @@ use std::{
     default::Default,
     fs::read_to_string,
 };
+use regex::*;
 use robuild::*;
 
 mod error;
 use error::*;
+mod ss;
+use ss::*;
 
 type RResult<'a, T> = result::Result::<T, RakeError<'a>>;
 
 struct Rakefile<'a> {
+    deps_re: Regex,
     file_path: String,
     jobs: Vec::<Job>,
     row: usize,
@@ -25,6 +29,7 @@ struct Rakefile<'a> {
 impl Default for Rakefile<'_> {
     fn default() -> Self {
         Self {
+            deps_re: Regex::new("").unwrap(),
             file_path: String::default(),
             jobs: Vec::default(),
             row: 1,
@@ -36,7 +41,9 @@ impl Default for Rakefile<'_> {
 impl<'a> Rakefile<'a> {
     pub const TAB_WIDTH: usize = 4;
     pub const MAX_DIR_LVL: usize = 2;
+
     pub const RAKE_FILE_NAME: &'static str = "Rakefile";
+    pub const DEPS_REGEX: &'static str = r"\$d\[(.*?)\]";
 
     fn find_rakefile() -> RResult::<'a, PathBuf> {
         let dir_path = env::current_dir().unwrap_or_report();
@@ -57,7 +64,6 @@ impl<'a> Rakefile<'a> {
     #[inline]
     fn advance(&mut self) {
         self.iter.next();
-        self.row += 1;
     }
 
     #[inline]
@@ -65,39 +71,59 @@ impl<'a> Rakefile<'a> {
         self.jobs.push(job);
     }
 
-    // Allow people to use both Makefiles and Rakefiles
-    // special symbols.
-    //
-    // SS -> Special Symbol
-    //
-    const TARGET_MAKE_SS: &'static str = "$@";
-    const TARGET_RAKE_SS: &'static str = "$t";
+    fn parse_deps_ss(&self, line: &str, deps: &Vec::<&str>) -> RResult::<String> {
+        for caps in self.deps_re.captures_iter(&line) {
+            let idx = caps[1].parse::<usize>().unwrap_or(0);
+            if deps.get(idx).is_none() {
+                return Err(RakeError::DepsIndexOutOfBounds(Info::from(self)));
+            }
+        }
 
-    const DEPS_MAKE_SS: &'static str = "$d";
-    const DEPS_RAKE_SS: &'static str = "$<";
+        let deps = self.deps_re.replace_all(&line, |caps: &Captures| {
+            let idx = caps[1].parse::<usize>().unwrap_or(0);
+            deps[idx]
+        }).to_string();
 
-    const ALL_DEPS_MAKE_SS: &'static str = "$ad";
-    const ALL_DEPS_RAKE_SS: &'static str = "$^";
+        Ok(deps)
+    }
 
     fn parse_special_symbols
     (
+        &self,
         target:      &str,
         deps_joined: &str,
         deps: &Vec::<&str>,
         line: &mut String
-    ) {
-        *line = line.replace(Self::TARGET_MAKE_SS, &target);
-        *line = line.replace(Self::TARGET_RAKE_SS, &target);
+    ) -> RResult::<()>
 
-        *line = line.replace(Self::DEPS_MAKE_SS, &deps[0]);
-        *line = line.replace(Self::DEPS_RAKE_SS, &deps[0]);
+    {
+        *line = self.parse_deps_ss(&line, &deps)?;
 
-        *line = line.replace(Self::ALL_DEPS_MAKE_SS, &deps_joined);
-        *line = line.replace(Self::ALL_DEPS_RAKE_SS, &deps_joined);
+        sreplace!(line, MakeTarget, &target);
+        sreplace!(line, RakeTarget, &target);
+
+        if line.contains(&SSymbol::MakeDep.to_string())
+        || line.contains(&SSymbol::RakeDep.to_string())
+        {
+            let Some(first_dep) = deps.get(0) else {
+                return Err(RakeError::DepsSSwithoutDeps(Info::from(self)))
+            };
+            sreplace!(line, MakeDep, &first_dep);
+            sreplace!(line, RakeDep, &first_dep);
+        }
+
+        sreplace!(line, MakeDeps, &deps_joined);
+        sreplace!(line, RakeDeps, &deps_joined);
+
+        Ok(())
     }
 
     fn parse_job(&mut self, idx: &usize, line: &str) -> RResult::<()> {
         let (target, deps_untrimmed) = line.split_at(*idx);
+
+        if target.trim().is_empty() {
+            return Err(RakeError::NoTarget(Info::from(self)))
+        }
 
         let deps_str = deps_untrimmed.chars()
             .skip_while(|c| c.is_whitespace() || c.eq(&':'))
@@ -111,8 +137,12 @@ impl<'a> Rakefile<'a> {
 
         let mut body = Vec::new();
         while let Some(next_line) = self.iter.peek() {
+            self.row += 1;
+
             let mut next_line = (*next_line).to_owned();
-            Self::parse_special_symbols(&target, &deps_joined, &deps, &mut next_line);
+            self.parse_special_symbols(&target, &deps_joined,
+                                       &deps, &mut next_line)
+                .unwrap_or_report();
 
             let trimmed = next_line.trim().to_owned();
 
@@ -132,13 +162,17 @@ impl<'a> Rakefile<'a> {
                     self.advance();
                     body.push(trimmed)
                 }
-                i @ 1.. => return Err(RakeError::InvalidIndentation(&self.file_path, i, self.row + 1)),
+                i @ 1.. => return Err(RakeError::InvalidIndentation(Info::from(self), i)),
                 _ => if trimmed.is_empty() { self.advance(); } else { break }
             };
         }
 
-        let mut cmd = RobCommand::new();
-        body.iter().for_each(|line| { cmd.append_mv(&[line]); });
+        let cmd = body.iter()
+            .fold(RobCommand::new(), |mut cmd, line| {
+                cmd.append_mv(&[line]);
+                cmd
+            });
+
         let job = Job::new(target, deps, cmd);
         self.append_job(job);
 
@@ -147,11 +181,9 @@ impl<'a> Rakefile<'a> {
 
     fn execute_jobs(&mut self) -> IoResult::<Vec::<Vec::<Output>>> {
         let mut jobss = Vec::new();
-
         for job in self.jobs.iter_mut() {
             jobss.push(job.execute_async()?);
         }
-
         Ok(jobss)
     }
 
@@ -166,6 +198,7 @@ impl<'a> Rakefile<'a> {
         let file_str = read_to_string(&file_path).unwrap_or_report();
 
         let mut rakefile = Rakefile {
+            deps_re: Regex::new(Self::DEPS_REGEX).unwrap(),
             file_path: Self::pretty_file_path(file_path.to_str().expect("Failed to convert file path to str")),
             iter: file_str.lines().peekable(),
             ..Self::default()
@@ -188,4 +221,4 @@ fn main() {
     2. Special symbols: $@, $t, $d, $<, $*, %, CC,
     3. Parse flags,
     4. Async mode,
-*/
+ */
